e1b4540855b799682698b44181d0803a8f2f5c70
farmanali11 <farmanalishar11@gmail.com>
2026-04-12 00:09:02 +0500

    feat: connect frontend to production backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://stylesync-server-2026.onrender.com/api'
 });
 
 // Automatically attach token to every request
